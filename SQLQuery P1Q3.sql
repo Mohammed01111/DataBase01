ecbbc6dfa5f8124ea04c48851523c3c7185eb0f7
Mohammed Almanwari <146930442+Mohammed01111@users.noreply.github.com>
2024-10-31 11:14:32 +0400

    Add files via upload

---
+++ SQLQuery P1Q3.sql
@@ -0,0 +1,80 @@
+
+USE HotelBookingSystems;
+
+-- Table for Hotels
+CREATE TABLE Hotels(
+	HotelID INT IDENTITY PRIMARY KEY ,
+	HotelName VARCHAR (255) NOT NULL UNIQUE,
+	Location VARCHAR(255) NOT NULL,
+    ContactNumber VARCHAR(15) NOT NULL,
+	Rating FLOAT CHECK (Rating >= 1 AND Rating <= 5)
+);
+
+-- Table for Rooms
+CREATE TABLE Rooms (
+    RoomID INT IDENTITY PRIMARY KEY,
+    RoomNumber VARCHAR(10) NOT NULL UNIQUE,
+    RoomType VARCHAR(50) NOT NULL CHECK (RoomType IN ('Single', 'Double', 'Suite')),
+    PricePerNight DECIMAL(10, 2) NOT NULL CHECK (PricePerNight > 0),
+    IsAvailable BIT NOT NULL DEFAULT 1,
+    HotelID INT,
+    FOREIGN KEY (HotelID) REFERENCES Hotels(HotelID) 
+);
+
+
+CREATE TABLE Guests (
+    GuestID INT IDENTITY PRIMARY KEY,
+    GuestName VARCHAR(255) NOT NULL,
+    Contact VARCHAR(255) NOT NULL,
+    IDProofType VARCHAR(50) NOT NULL,
+    IDProofNumber VARCHAR(50) NOT NULL
+);
+
+
+-- Table for Bookings
+CREATE TABLE Bookings (
+    BookingID INT IDENTITY PRIMARY KEY,
+    BookingDate DATETIME NOT NULL,
+    CheckInDate DATETIME NOT NULL,
+    CheckOutDate DATETIME NOT NULL,
+    Status VARCHAR(20) NOT NULL CHECK (Status IN ('Pending', 'Confirmed', 'Canceled', 'Check-in', 'Check-out')) DEFAULT 'Pending',
+    TotalCost DECIMAL(10, 2) NOT NULL,
+    RoomID INT,
+    GuestID INT,
+    FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID),
+    FOREIGN KEY (GuestID) REFERENCES Guests(GuestID) 
+);
+
+
+
+-- Table for Payments
+CREATE TABLE Payments (
+    PaymentID INT IDENTITY PRIMARY KEY,
+    PaymentDate DATETIME NOT NULL,
+    Amount DECIMAL(10, 2) NOT NULL CHECK (Amount > 0),
+    PaymentMethod VARCHAR(50),
+    BookingID INT,
+    FOREIGN KEY (BookingID) REFERENCES Bookings(BookingID) 
+);
+SELECT BookingID FROM Bookings;
+-- Table for Staff Members
+CREATE TABLE StaffMembers (
+    StaffID INT IDENTITY PRIMARY KEY,
+    StaffName VARCHAR(255) NOT NULL,
+    Position VARCHAR(50) NOT NULL,
+    ContactNumber VARCHAR(15) NOT NULL,
+    HotelID INT,
+    FOREIGN KEY (HotelID) REFERENCES Hotels(HotelID) 
+);
+
+-- Table for Reviews
+CREATE TABLE Reviews (
+    ReviewID INT IDENTITY PRIMARY KEY,
+    Rating INT CHECK (Rating BETWEEN 1 AND 5),
+    Comments VARCHAR(255) DEFAULT 'No comments',
+    ReviewDate DATETIME NOT NULL,
+    HotelID INT,
+    GuestID INT,
+    FOREIGN KEY (HotelID) REFERENCES Hotels(HotelID) ,
+    FOREIGN KEY (GuestID) REFERENCES Guests(GuestID) 
+);
---
+++ SQLQuery P2.sql
@@ -0,0 +1,223 @@
+--Part 2 
+--Indexing
+CREATE NONCLUSTERED INDEX idx_hotel_name
+ON Hotels(HotelName);
+
+CREATE NONCLUSTERED INDEX idx_hotel_rating
+ON Hotels(Rating);
+
+
+
+CREATE CLUSTERED INDEX idx_hotel_room 
+ON Rooms (HotelID,RoomNumber);
+
+CREATE NONCLUSTERED INDEX idx_room_type
+ON Rooms (RoomType);
+
+
+
+
+CREATE NONCLUSTERED INDEX idx_GuestID_ 
+ON Bookings (GuestID);
+
+CREATE NONCLUSTERED INDEX idx_Status 
+ON Bookings (Status);
+
+CREATE NONCLUSTERED INDEX idx_Room_Check
+ON Bookings (RoomID, CheckInDate, CheckOutDate);
+
+
+
+
+--views
+--1
+CREATE VIEW ViewTopRatedHotels 
+AS
+SELECT 
+    h.HotelID AS [HotelID],h.HotelName AS [HotelName],COUNT(r.RoomID) AS TotalRooms,AVG(r.PricePerNight) AS AverageRoomPrice
+FROM 
+    Hotels h
+JOIN 
+    Rooms r ON h.HotelID = r.HotelID
+WHERE 
+    h.Rating > 4.5
+GROUP BY 
+    h.HotelID, h.HotelName;
+
+
+select * from ViewTopRatedHotels;
+
+--2
+CREATE VIEW ViewGuestBookings AS
+SELECT 
+    g.GuestID AS GuestID,g.GuestName AS GuestName,COUNT(b.BookingID) AS TotalBookings,SUM(b.TotalCost) AS TotalSpent
+FROM 
+    guests g
+LEFT JOIN 
+    bookings b ON g.GuestID = b.GuestID
+GROUP BY 
+    g.GuestID, g.GuestName;
+
+select * from ViewGuestBookings
+
+
+
+CREATE VIEW ViewAvailableRooms AS
+SELECT 
+    h.HotelID AS HotelID,
+    h.HotelName AS HotelName,
+    r.RoomType AS RoomType,
+    r.PricePerNight AS PricePerNight
+FROM 
+    hotels h
+JOIN 
+    rooms r ON h.HotelID = r.HotelID
+WHERE 
+    r.IsAvailable = 1  
+GROUP BY 
+    h.HotelID, h.HotelName, r.RoomType, r.PricePerNight;
+
+
+select * from ViewAvailableRooms
+
+
+
+
+CREATE VIEW ViewBookingSummary AS
+SELECT 
+    h.HotelID AS HotelID,
+    h.HotelName AS HotelName,
+    COUNT(b.BookingID) AS TotalBookings,
+    SUM(CASE WHEN b.status = 'Confirmed' THEN 1 ELSE 0 END) AS ConfirmedBookings,
+    SUM(CASE WHEN b.status = 'Pending' THEN 1 ELSE 0 END) AS PendingBookings,
+    SUM(CASE WHEN b.status = 'Canceled' THEN 1 ELSE 0 END) AS CanceledBookings
+FROM 
+    hotels h
+LEFT JOIN 
+    bookings b ON h.HotelID = b.HotelHD  
+GROUP BY 
+    h.HotelID, h.HotelName;
+
+
+SELECT * FROM ViewBookingSummary
+
+
+
+
+
+
+
+
+--Functions
+CREATE FUNCTION GetHotelAverageRating(@HotelID INT)
+RETURNS FLOAT
+AS
+BEGIN
+    RETURN (SELECT AVG(Rating) FROM reviews WHERE HotelID = @HotelID);
+END;
+
+
+
+
+
+CREATE FUNCTION GetNextAvailableRoom(@HotelID INT, @RoomType VARCHAR(50))
+RETURNS INT
+AS
+BEGIN
+    RETURN (SELECT TOP 1 RoomID FROM rooms WHERE HotelID = @HotelID AND RoomType= @RoomType AND IsAvailable = 1);
+END;
+
+
+
+CREATE FUNCTION CalculateOccupancyRate(@HotelID INT)
+RETURNS FLOAT
+AS
+BEGIN
+    DECLARE @OccupiedRooms INT = (SELECT COUNT(DISTINCT RoomID) FROM bookings WHERE HotelID = @HotelID AND CheckInDate >= DATEADD(DAY, -30, GETDATE()) AND status = 'Confirmed');
+    DECLARE @TotalRooms INT = (SELECT COUNT(*) FROM rooms WHERE HotelID = @HotelID);
+
+    RETURN CASE WHEN @TotalRooms = 0 THEN 0 ELSE CAST(@OccupiedRooms AS FLOAT) / @TotalRooms * 100 END;
+END;
+
+
+
+
+
+
+--Stored Procedures
+CREATE PROCEDURE sp_MarkRoomUnavailable
+    @RoomID INT
+AS
+BEGIN
+    UPDATE rooms SET IsAvailable = 0 WHERE RoomID = @RoomID;
+END;
+
+
+
+
+
+CREATE PROCEDURE sp_UpdateBookingStatus
+    @BookingID INT,
+    @NewStatus VARCHAR(20)
+AS
+BEGIN
+    UPDATE bookings SET status = @NewStatus WHERE BookingID = @BookingID;
+END;
+
+
+
+CREATE PROCEDURE sp_RankGuestsBySpending
+AS
+BEGIN
+    SELECT g.GuestID, g.GuestName, SUM(b.TotalCost) AS TotalSpent,
+           RANK() OVER (ORDER BY SUM(b.total_amount) DESC) AS Rank
+    FROM guests g
+    LEFT JOIN bookings b ON g.GuestID = b.GuestID
+    GROUP BY g.GuestID, g.GuestName;
+END;
+
+
+
+
+--Triggers
+
+CREATE TRIGGER trg_UpdateRoomAvailability
+ON bookings
+AFTER INSERT
+AS
+BEGIN
+    UPDATE rooms SET IsAvailable = 0 WHERE RoomID IN (SELECT RoomID FROM inserted);
+END;
+
+
+
+
+CREATE TRIGGER trg_CalculateTotalRevenue
+ON payments
+AFTER INSERT
+AS
+BEGIN
+    UPDATE hotels SET total_revenue = total_revenue + (SELECT SUM(amount) FROM inserted) 
+    WHERE HotelID IN (SELECT HotelID FROM bookings WHERE BookingID IN (SELECT BookingID FROM inserted));
+END;
+
+
+
+CREATE TRIGGER trg_CheckInDateValidation
+ON bookings
+INSTEAD OF INSERT
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM inserted WHERE CheckInDate > CheckOutDate)
+    BEGIN
+        RAISERROR('Check-in date cannot be greater than check-out date.', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+    ELSE
+    BEGIN
+        INSERT INTO bookings (CheckInDate) SELECT CheckInDate FROM inserted;
+    END
+END;
+
+
+
